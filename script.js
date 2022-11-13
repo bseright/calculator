1f8dfc2e96b700021ecbb2b2d86236f778f4cc06
Britain Seright <britainseright@gmail.com>
2022-11-12 22:22:34 -0600

    append next button press and reset alert with button

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-let current;
+let current = 0;
+
 let buttons = document.querySelectorAll("button");
+let alertText = document.querySelector("p");
+
+alertText.textContent = current;
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        console.log(this.id)
+
+        if (this.id === "reset") {
+
+            current = 0;
+            alertText.textContent = current;
+
+        } else if (current === 0 && this.id == 0) {
+        
+        } else if (current === 0) {
+
+            current = this.id;
+            alertText.textContent = current;
+
+        } else {
+
+            current = current.toString() + this.id.toString();
+            alertText.textContent = current;
+
+        }
     })
 });
